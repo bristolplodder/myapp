0ac1ee07b962b117483dbe88c4ed7768bd68c9ab
BCC_MattC <matthew.cockburn@bristol.gov.uk>
2013-08-23 07:32:08 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'cc89faed8285778e053ba3ea31edacf5848c9c2de5257f64ce347c06a210e2aacd8d73b1659bc924a2708cf74055b11e4afe79c1ef4549e19089cb132496f635'
